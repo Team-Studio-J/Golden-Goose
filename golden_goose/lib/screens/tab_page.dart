2cc8765614655ac0c07c42c2c6b760daeadc1d6a
kcoms555 <kcoms555@naver.com>
2021-11-13 19:30:52 +0900

    feat: change tab as swipable property

--- golden_goose/lib/screens/tab_page.dart
+++ golden_goose/lib/screens/tab_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:golden_goose/controllers/tab_page_controller.dart';
-import 'package:get/get.dart';
-import 'package:golden_goose/screens/home.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:golden_goose/controllers/tab_page_controller.dart';
+import 'package:golden_goose/screens/home.dart';
 
+import 'chart.dart';
 
-class TabPage extends StatefulWidget { // StatefulWidget
+class TabPage extends StatefulWidget {
+  // StatefulWidget
 
   @override
   _TabPageState createState() => _TabPageState();
 }
 
 class _TabPageState extends State<TabPage> {
-
   final TabPageController _tabPageCtrl = Get.put(TabPageController());
-  List? _pages;
+  List<Widget>? _pages;
 
   @override
   void initState() {
@@ -23,12 +24,13 @@ class _TabPageState extends State<TabPage> {
     super.initState();
     _pages = [
       Home(),
-      Text('Study Page'),
+      Chart(),
       Text('Manage Page'),
       Text('Test Page'),
     ];
   }
 
+  /*
   @override
   Widget build(BuildContext context) {
     print('tabPage >> build');
@@ -42,10 +44,10 @@ class _TabPageState extends State<TabPage> {
             selectedFontSize: 10,
             unselectedFontSize: 8,
             //showUnselectedLabels: false,
-            backgroundColor: Get.theme.backgroundColor.withOpacity(0.2),
+            backgroundColor: Get.theme.backgroundColor,
             selectedItemColor: Colors.white,
             unselectedItemColor: Colors.grey,
-            type: BottomNavigationBarType.fixed,
+            type: BottomNavigationBarType.shifting,
             // This is all you need!
             onTap: _tabPageCtrl.changeCurPage, //_onItemTapped,
             items:[
@@ -67,5 +69,47 @@ class _TabPageState extends State<TabPage> {
       ),
     ));
   }
-
+  */
+  @override
+  Widget build(BuildContext context) {
+    print('tabPage >> build');
+    return DefaultTabController(
+        length: _pages!.length,
+        child: Scaffold(
+          body: TabBarView(
+            physics: BouncingScrollPhysics(),
+            children: _pages!,
+          ),
+          bottomNavigationBar: TabBar(
+              isScrollable: false,
+              indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                      width: 5.0, color: Colors.white.withOpacity(0.1)),
+                  insets: EdgeInsets.symmetric(horizontal: 16.0)),
+              padding: EdgeInsets.zero,
+              indicatorPadding: EdgeInsets.zero,
+              tabs: [
+                Tab(
+                    height: 40,
+                    iconMargin: EdgeInsets.zero,
+                    icon: Icon(Icons.home, size: 20),
+                    child: Text("home", style: TextStyle(fontSize: 10))),
+                Tab(
+                    height: 40,
+                    iconMargin: EdgeInsets.zero,
+                    icon: Icon(Icons.waterfall_chart, size: 20),
+                    child: Text("Chart", style: TextStyle(fontSize: 10))),
+                Tab(
+                    height: 40,
+                    iconMargin: EdgeInsets.zero,
+                    icon: Icon(FontAwesomeIcons.alignLeft, size: 20),
+                    child: Text("Rank", style: TextStyle(fontSize: 10))),
+                Tab(
+                    height: 40,
+                    iconMargin: EdgeInsets.zero,
+                    icon: Icon(Icons.history, size: 20),
+                    child: Text("History", style: TextStyle(fontSize: 10))),
+              ]),
+        ));
+  }
 }
